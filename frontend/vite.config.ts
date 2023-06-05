b2f9b6f5fa281129d05bccdcfa30e202acb70821
Jason <jason.stuart27@gmail.com>
2023-06-05 14:43:21 +0930

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,15 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import { defineConfig } from 'vite';
-
+import react from '@vitejs/plugin-react'
 export default defineConfig({
   define: {
     "global": {},
+  },
+  plugins: [react()],
+  build: {
+    commonjsOptions: { include: [] },
+  },
+  optimizeDeps: {
+    disabled: false,
   },
 });
