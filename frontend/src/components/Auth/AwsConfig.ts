aa2cd573c33036e28abfc57e6b9c11c29e9788df
Jason <jason.stuart27@gmail.com>
2023-06-05 15:20:10 +0930

    y

--- frontend/src/components/Auth/AwsConfig.ts
+++ frontend/src/components/Auth/AwsConfig.ts
@@ -1,38 +1,11 @@
 import { Amplify, Auth } from 'aws-amplify';
-import { SSM } from 'aws-sdk';
-//import AWS from './config/aws'
-
-// Create an instance of the SSM client
-const ssm = new SSM({region: 'ap-southeast-2'});
-const userPoolIdParam = '/TodoList/dev/userPoolId';
-const userPoolWebClientIdParam = '/TodoList/dev/userPoolClient';
-
-async function getParameter(parameterName: string): Promise<string> {
+ function configureAmplify() {
   try {
-    const request: SSM.GetParameterRequest = {
-      Name: parameterName,
-      WithDecryption: true
-    };
-
-    const response = await ssm.getParameter(request).promise();
-
-    return response.Parameter?.Value || '';
-  } catch (error) {
-    console.error('Failed to retrieve the parameter:', error);
-    throw error;
-  }
-}
-
-async function configureAmplify() {
-  try {
-    
-    const userPoolId = await getParameter(userPoolIdParam);
-    const userPoolWebClientId = await getParameter(userPoolWebClientIdParam);
-    Amplify.configure({
+   Amplify.configure({
       Auth: {
-        region: 'ap-southeast-2',
-        userPoolId: userPoolId,
-        userPoolWebClientId: userPoolWebClientId,
+        region: import.meta.env.VITE_AWS_REGION,
+        userPoolId: import.meta.env.VITE_AWS_USERPOOL_ID,
+        userPoolWebClientId: import.meta.env.VITE_AWS_USERPOOL_WEBCLIENT_ID,
         mandatorySignIn: true,
       }
     });
@@ -46,8 +19,8 @@ async function configureAmplify() {
   }
 }
 
-export default async function initializeAmplify() {
-  const currentConfig = await configureAmplify();
+export default function initializeAmplify() {
+  const currentConfig = configureAmplify();
   
   // Perform any additional initialization or setup here
   return currentConfig;
